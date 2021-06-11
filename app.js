4a18cc089e875405f9e7b1e4c3324f025f755a0b
Junior Freitas <joaofbjunior01@gmail.com>
2021-06-11 00:13:03 -0300

    Modelando de acordo com Ecommerce, temporariamente até que o webscrapper esteja pronto

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const app = express();
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
+
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos')
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Acesss-Control-Allow-Header', 'Content-Type, Origin, X-Requested-With, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Acess-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).send({});
+
+  }
+  next();
+})
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
+
 // Retorna produtos
 router.get('/', (req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Usando o GET dentro da rota de produtos'
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'SELECT * FROM produtos;',
+      (error, resultado, fields) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        const response = {
+          quantidade: resultado.length,
+          produtos: resultado.map(prod => {
+            return {
+              id_produto: prod.id_produto,
+              nome: prod.nome,
+              preco: prod.preco,
+              request: {
+                tipo: 'GET',
+                descricao: '',
+                url: `http://localhost:3000/produtos/${prod.id_produto}`
+              }
+            }
+          })
+        }
+        return (res.status(200).send({ response }))
+      }
+    )
   })
 })
 // Insere produtos
@@ -12,38 +37,99 @@ router.post('/', (req, res, next) => {
     nome: req.body.nome,
     preco: Number(req.body.preco),
   }
-  res.status(201).send({
-    mensagem: 'Cadastro de produto efetivado. ',
-    produtoCriado: `Nome: ${produto.nome} Preço: ${produto.preco >= 0 ? (produto.preco) : ('Produto Invalido')}`
+  if (produto.preco >= 0 || produto.preco <= 0) {
+
+  } else {
+    return res.status(501).send({
+      error: 'Produto com preço invalido'
+    })
+  }
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'INSERT INTO produtos(nome,preco) VALUES(?,?)',
+      [produto.nome, produto.preco],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({
+            error: error,
+            response: null
+          })
+        }
+        res.status(201).send({
+          mensagem: `Cadastro de produto feito com o ID ${resultado.insertId}`,
+          produtoCriado: `Nome: ${produto.nome} Preço: ${produto.preco >= 0 ? (produto.preco) : ('Produto Invalido')}`
+        })
+
+      }
+    )
   })
+
 })
 // Retorna dados de um produto
 router.get('/:id_produto', (req, res, next) => {
   const id = req.params.id_produto
-  if (id === 'especial') {
-    res.status(200).send({
-      mensagem: 'Usando o GET de um produto ESPECIAL',
-      id: id
-    })
-  } else {
-    res.status(200).send({
-      mensagem: 'Usando o GET de um produto exclusivo',
-      id: id
-    })
-  }
-
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'SELECT * FROM produtos WHERE id_produto = ?',
+      [id],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        return (res.status(200).send({ response: resultado }))
+      }
+    )
+  })
 })
+
 // Alterando um produto
-router.patch('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Usando o PATCH dentro da rota de produtos'
+router.patch('/:id_produto', (req, res, next) => {
+  const id = req.params.id_produto
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      `UPDATE produtos
+        SET nome = ?,
+          preco = ?
+      WHERE id_produto = ?;
+      `,
+      [req.body.nome, req.body.preco, id],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+          'SELECT * FROM produtos WHERE id_produto = ?',
+          [id],
+          (error, resultado, field) => {
+            if (error) { return res.status(500).send({ error: error }) }
+            return (res.status(200).send({ novos_valores: resultado }))
+          }
+        )
+
+      }
+    )
   })
 })
 // Deletando um produto
-router.delete('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Usando o DELETE dentro da rota de produtos'
+router.delete('/:id_produto', (req, res, next) => {
+  id = req.params.id_produto;
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'DELETE FROM produtos WHERE id_produto = ?',
+      [id],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        return (res.status(202).send({
+          resultado: resultado
+        }))
+      }
+    )
   })
+
 })
 
 module.exports = router;
